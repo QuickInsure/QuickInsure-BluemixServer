bcb06efe6626361be2b3efb2cf5ce8ab79c644bb
Avdhut Vaidya <avdhut.vaidya@gmail.com>
2016-03-18 16:25:59 +0530

    Modified dummy data in mobileAuth method

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -10,7 +10,7 @@ class LoginController < ApplicationController
 			flag = nil
 		else
 			flag = false
-			if username == "avdhut.vaidya" && password == "123456"
+			if username == "avdhut.vaidya" && password == "Avdhut@2016"
 				flag = true
 			end
 		end
