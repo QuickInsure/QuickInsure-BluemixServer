5efb28e920809c8908e48f750b48cbd47a747eb1
Avdhut V. Vaidya <avdhut.vaidya@gmail.com>
2016-03-26 21:45:43 +0530

    Added nil check

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -89,7 +89,7 @@ class LoginController < ApplicationController
 			requestStr = URI.parse("http://generalinsurance.mybluemix.net/banking/icicibank_general_insurance/getCustomerDtls?#{reqParams.to_query}")
 			puts requestStr
 			response = JSON.parse(Net::HTTP.get(requestStr))
-			responseHash[:code] = response[0]["code"]
+			responseHash[:code] = response[0]["code"] if response[0]
 			responseHash[:policyData] = response[1]
 			responseHash = responseHash.to_json
         end
