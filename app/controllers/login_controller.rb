3fe171d2f6d8ed4692c2cc92b4a033fdc2864d05
Avdhut V. Vaidya <avdhut.vaidya@gmail.com>
2016-03-27 09:50:31 +0530

    Added nil check

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -83,7 +83,7 @@ class LoginController < ApplicationController
 			requestStr = URI.parse("http://retailbanking.mybluemix.net/banking/icicibank/account_summary?#{reqParams.to_query}")
 			puts requestStr
 			response = JSON.parse(Net::HTTP.get(requestStr))
-			responseHash[:code] = response["code"]
+			responseHash[:code] = response[0]["code"]
 			responseHash = {:userData=>response[1]}
 
 			reqParams = {:client_id => $client_id.to_s, :token => $token.to_s, :mobileNo => "9820120461", :emailId => "avdhut.vaidya@gmail.com"}
